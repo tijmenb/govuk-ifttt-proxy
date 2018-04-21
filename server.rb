a284d06c2516d90c0c8e2de36debc02548b66567
Tijmen Brommet <tijmen@gmail.com>
2018-04-21 23:15:43 +0100

    Add setup endpoint

--- server.rb
+++ server.rb
@@ -15,3 +15,19 @@ get '/ifttt/v1/status' do
     }
   }.to_json
 end
+
+post '/ifttt/v1/test/setup' do
+  halt 401 unless request.env.fetch('HTTP_IFTTT_CHANNEL_KEY') == ENV["IFFT_SERVICE_KEY"]
+
+  {
+    "data": {
+      "samples": {
+        "triggers": {
+          "search-trigger": {
+            "keywords": ""
+          }
+        }
+      }
+    }
+  }.to_json
+end
