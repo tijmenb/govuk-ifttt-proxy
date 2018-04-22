46c471f326059265a3cdb315bc77f9ecbfb55542
Tijmen Brommet <tijmen@gmail.com>
2018-04-22 08:11:41 +0100

    Simplify status endpoint

--- server.rb
+++ server.rb
@@ -3,17 +3,7 @@ require 'sinatra'
 get '/ifttt/v1/status' do
   halt 401 unless request.env.fetch('HTTP_IFTTT_CHANNEL_KEY') == ENV["IFFT_SERVICE_KEY"]
 
-  {
-    "data": {
-      "samples": {
-        "triggers": {
-          "search-trigger": {
-            "keywords": ""
-          }
-        }
-      }
-    }
-  }.to_json
+  { status: "OK" }.to_json
 end
 
 post '/ifttt/v1/test/setup' do
