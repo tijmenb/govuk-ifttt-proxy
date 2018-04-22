94d6d55990c9449b37cbb431223afbebc6a09989
Tijmen Brommet <tijmen@gmail.com>
2018-04-22 08:17:17 +0100

    Expose search results

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'http'
 
 get '/ifttt/v1/status' do
   halt 401 unless request.env.fetch('HTTP_IFTTT_CHANNEL_KEY') == ENV["IFFT_SERVICE_KEY"]
@@ -25,7 +26,12 @@ end
 post '/ifttt/v1/triggers/search-trigger' do
   halt 401 unless request.env.fetch('HTTP_IFTTT_CHANNEL_KEY') == ENV["IFFT_SERVICE_KEY"]
 
-  entries = []
+  data = JSON.parse(request.body.read)
+  keywords = data["triggerFields"]["keywords"]
+
+  response = HTTP.get("http://gov.uk/api/search.json?q=#{keywords}")
+
+  entries = response["results"]
 
   {
     "data": entries,
