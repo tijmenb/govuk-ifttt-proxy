cf13289b295707aac9536eef0b9edda1283a5ef2
Tijmen Brommet <tijmen@gmail.com>
2018-04-22 11:53:04 +0100

    Add another test example

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-curl -H "Content-Type: application/json; IFTTT-Service-Key: X;" -X POST -d '{"triggerFields":{"keywords":"tax"}}' http://localhost:4567/ifttt/v1/triggers/search-trigger;
+# curl -H "Content-Type: application/json; IFTTT-Service-Key: X;" -X POST -d '{"triggerFields":{"keywords":"tax"}}' http://localhost:4567/ifttt/v1/triggers/search-trigger;
+curl -H "Content-Type: application/json; IFTTT-Service-Key: X;" -X POST -d '{"triggerFields":{"company_number":"09426399"}}' http://localhost:4567/ifttt/v1/triggers/companies;
